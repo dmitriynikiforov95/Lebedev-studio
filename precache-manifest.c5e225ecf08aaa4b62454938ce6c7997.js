cc3a92906cac8d64d1769f785d47e32dda292c00
Dmitriy Nikiforov <nikiforov.psy@gmail.com>
2020-06-29 10:21:40 +0300

    Updates

---
+++ precache-manifest.c5e225ecf08aaa4b62454938ce6c7997.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dd347a6ee75849c733a4008d32aae249",
+    "url": "/lebedev-studio/index.html"
+  },
+  {
+    "revision": "b86ced0958661c20d0dc",
+    "url": "/lebedev-studio/static/css/main.0df9c1b3.chunk.css"
+  },
+  {
+    "revision": "089c7f1f8437e90dba0f",
+    "url": "/lebedev-studio/static/js/2.740ac2a9.chunk.js"
+  },
+  {
+    "revision": "3d66a4ed243bca57a1e68168bbc08249",
+    "url": "/lebedev-studio/static/js/2.740ac2a9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b86ced0958661c20d0dc",
+    "url": "/lebedev-studio/static/js/main.2901601d.chunk.js"
+  },
+  {
+    "revision": "a4fc0298aee9e97bcc65",
+    "url": "/lebedev-studio/static/js/runtime-main.adf74c5a.js"
+  }
+]);
